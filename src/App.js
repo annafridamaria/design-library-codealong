4099625b42b8d70d243bcd3d5e1fb362883cd92a
Frida Jonsson <annafridamariajonsson@gmail.com>
2020-01-22 12:00:28 +0100

    half codealong

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Card } from 'lib/Card'
 
 export const App = () => {
   return (
     <div>
-      Find me in src/app.js!
+      <Card 
+      coverImage="https://placebear.com/g/200/300"
+      title="Orcas" 
+      secondaryText="Orcas are found to be one of the most psychological intresting animals on earth"
+      thumbnailUrl="https://www.fillmurray.com/g/100/100"
+      />
     </div>
   )
 }
